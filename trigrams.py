33a6748cd0adc807110a1a07d05aa8a1b38f276f
Michael Shinners <mishinners@icloud.com>
2017-10-19 14:06:19 -0700

    touched begining files and started sudo code

---
+++ trigrams.py
@@ -0,0 +1,18 @@
+"""This module will create a function that will take the first 2 words from a given block of text and return the immidately following word in all possible places."""
+
+
+def main():
+
+
+
+
+
+Create a main function which implements the core of your algorithm. It should take two arguments, the path to a source file and an integer representing the number of words to generate.
+
+Try to keep your code well-factored by creating functions to execute discreet steps of the processing. You might have one function that processes the input into your trigram source. Perhaps there is another that is responsible for selecting a new word, given a pair of input words. Maybe there are others.
+
+
+
+
+
+if __name__ == '__main__': 
