ecdf7edbb1a99e49d5700a1d67ac65dd7a0415ef
Michael Shinners <mishinners@icloud.com>
2017-10-20 14:06:12 -0700

    Created and set up setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Our setup.py for future use."""
+
+
+from setuptools import setup
+
+setup(
+    name="trigram",
+    description="A Python implementation of a trigram parser.",
+    version=0.1,
+    author="Zach Taylor & Michael Shinners",
+    author_email="zacharymtaylor3@gmail.com & michaelshinners@gmail.com",
+    license='MIT',
+    py_modules=['trigram'],
+    install_requires=[],
+    extras_require={'test': ['pytest', 'pytest-cov']}
+)
