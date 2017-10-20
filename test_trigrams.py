54764286a1beb4e380faa1aa39df2d64c8c0f40b
Michael Shinners <mishinners@icloud.com>
2017-10-20 15:00:51 -0700

    2 tests written and successfully tested

--- test_trigrams.py
+++ test_trigrams.py
@@ -1,13 +1,27 @@
 """Test file for our Trigram program."""
 
 
-test for main
+import pytest
 
 
-test for generate_dictionary_from_source_text
+s = "key word value key word kiwi"
+result = {'key word': ['value', 'kiwi'], 'word value': ['key'], 'value key': ['word']}
 
 
-test for generate_trigrams_output
+def test_generate_dictionary_from_source_text():
+    """Function that tests our app runs the generate_dictionary_from_source_text function"""
+    from trigrams import generate_dictionary_from_source_text
+    assert generate_dictionary_from_source_text(s) == result
 
 
-tets for pick_word_in_dictionary
+s_b = 'a b c'
+result_b = 'a b c'
+
+
+def test_generate_trigrams_output():
+    """"Tests that our listed function produced the desired output."""
+    from trigrams import generate_trigrams_output
+    assert generate_trigrams_output(s_b, 3) == result_b
+
+
+# tets for pick_word_in_dictionary
